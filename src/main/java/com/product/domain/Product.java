6865e969e21d041b65345a93423fa14d10123a61
Luram Archanjo <luram.archanjo@sensedia.com>
2019-05-20 22:14:50 -0300

    Product draft

--- src/main/java/com/product/domain/Product.java
+++ src/main/java/com/product/domain/Product.java
@@ -1,5 +1,70 @@
 package com.product.domain;
 
+import java.util.Objects;
+import org.jnosql.artemis.Column;
+import org.jnosql.artemis.Entity;
+import org.jnosql.artemis.Id;
+
+@Entity(value = "product")
 public class Product {
+
+  @Id
+  private String id;
+
+  @Column
+  private String name;
+
+  @Column
+  private Integer quantity;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Product product = (Product) o;
+    return Objects.equals(id, product.id);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id);
+  }
+
+  @Override
+  public String toString() {
+    return "Product{" +
+        "id='" + id + '\'' +
+        ", name='" + name + '\'' +
+        ", quantity=" + quantity +
+        '}';
+  }
 
 }
